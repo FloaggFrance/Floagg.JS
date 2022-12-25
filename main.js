18a078e4c8280bcecedcaae0baca8c6ca63263f9
Floagg France <109751867+FloaggFrance@users.noreply.github.com>
2022-12-25 14:11:08 +0100

    up

---
+++ main.js
@@ -0,0 +1,6 @@
+setEE = null
+var setEventElement = function (element) { setEE = element; return setEE; }
+
+var event = function (nameEvent, functionName, eventElement=setEE) {
+	eventElement.addEventListener(nameEvent, functionName)
+}
