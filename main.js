fad841276970adb9e3506d47200dd32b5993667e
Floagg France <109751867+FloaggFrance@users.noreply.github.com>
2022-12-25 14:15:50 +0100

    up

--- main.js
+++ main.js
@@ -4,3 +4,8 @@ var setEventElement = function (element) { setEE = element; return setEE; }
 var event = function (nameEvent, functionName, eventElement=setEE) {
 	eventElement.addEventListener(nameEvent, functionName)
 }
+
+var css = function (name, up = null) {
+	if(up !== null) style.name = up
+	return style.name
+}
